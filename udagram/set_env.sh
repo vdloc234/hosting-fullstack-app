aab991e0619c27ca99e634635294485cbd6995f2
Loc <danhlocvuong@gmail.com>
2023-07-17 22:04:29 +0700

    update screenshots

--- udagram/set_env.sh
+++ udagram/set_env.sh
@@ -1,8 +1,8 @@
 # This file is used for convenience of local development.
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
-export POSTGRES_USERNAME=postgres
-export POSTGRES_PASSWORD=myPassword
-export POSTGRES_HOST=mydbinstance.csxbuclmtj3c.us-east-1.rds.amazonaws.com
+export POSTGRES_USERNAME=postgres_udagram
+export POSTGRES_PASSWORD=udagram_db_pass
+export POSTGRES_HOST=udagramdb-1.cxzlechiigbp.us-east-1.rds.amazonaws.com
 export POSTGRES_DB=postgres
 export AWS_BUCKET=arn:aws:s3:::udagrams3-903752074010
 export AWS_REGION=us-east-1
